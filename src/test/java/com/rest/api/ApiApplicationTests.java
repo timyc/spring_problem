ffb801acb3acb267b5f464abf4a302a60da04388
Tim Chen <29265905+timyc@users.noreply.github.com>
2023-03-14 20:46:38 -0700

    init

---
+++ src/test/java/com/rest/api/ApiApplicationTests.java
@@ -0,0 +1,20 @@
+package com.rest.api;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ApiApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+}
